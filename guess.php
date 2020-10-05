81b49d5fef368c3ef0467f7313b8b287dcb39c55
Eddy Mac <eddymac11@outlook.com>
2020-10-05 23:19:41 +0300

    Added php

--- guess.php
+++ guess.php
@@ -6,6 +6,25 @@
 <body>
 <h1>Welcome to this guessing game</h1>
 <p>Guess your number and have it in the url</p>
-<p>Feedback from server will show here</p>
+
+<?php
+$message;
+
+if (!isset($_GET['guess'])) {
+ 	$message = "Missing guess parameter";
+} elseif ($_GET['guess'] == "") {
+	$message = "Your guess is too short";
+} elseif (!is_numeric($_GET['guess'])) {
+	$message = "Your guess is not a number";
+} elseif ($_GET['guess'] < 36) {
+	$message = "Your guess is too low";
+} elseif ($_GET['guess'] > 36) {
+	$message = "Your guess is too high";
+} else {
+	$message = "Congratulations - You are right";
+}
+?>
+
+<p><?php echo $message ?></p>
 </body>
 </html>
