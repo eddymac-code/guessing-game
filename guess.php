345439a1955b07da9585ad7e1018ffabae0482a8
Eddy Mac <eddymac11@outlook.com>
2020-10-05 23:07:43 +0300

    Added some guiding htm

---
+++ guess.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simple Guessing Game</title>
+</head>
+<body>
+<h1>Welcome to this guessing game</h1>
+<p>Guess your number and have it in the url</p>
+<p>Feedback from server will show here</p>
+</body>
+</html>
